63de51e7911c938c03f17a29829632536ca235e6
Noah Pinales <pinales@usc.edu>
2026-01-04 12:38:29 -0800

    formatting & firebase versions

--- lib/game_route.dart
+++ lib/game_route.dart
@@ -15,77 +15,28 @@ class GameRoute extends StatelessWidget {
     final theme = Theme.of(context);
     final bool isDarkMode = theme.brightness == Brightness.dark;
     return Scaffold(
-        body: SafeArea(
-          bottom: false,
-          child: Column(
-            children: [
-              Expanded(
-                child: InAppWebView(
-                  initialSettings: InAppWebViewSettings(
-                    isInspectable: kDebugMode,
-                  ),
-                  initialUrlRequest:
-                      URLRequest(url: WebUri(gameUrl ?? "")),
-                  onWebViewCreated: (controller) {},
-                  onLoadStart: (controller, url) {},
-                  onLoadStop: (controller, url) async {
-                    await controller.evaluateJavascript(
-                        source: "hideHeader(); hideBackButton(); ${isDarkMode ? "enableDarkMode();" : ""}");
-                   
-                  },
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Expanded(
+              child: InAppWebView(
+                initialSettings: InAppWebViewSettings(
+                  isInspectable: kDebugMode,
                 ),
+                initialUrlRequest: URLRequest(url: WebUri(gameUrl ?? "")),
+                onWebViewCreated: (controller) {},
+                onLoadStart: (controller, url) {},
+                onLoadStop: (controller, url) async {
+                  await controller.evaluateJavascript(
+                      source:
+                          "hideHeader(); hideBackButton(); ${isDarkMode ? "enableDarkMode();" : ""}");
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        // bottomNavigationBar: Container(
-        //   decoration: BoxDecoration(
-        //     border: Border(
-        //       top: BorderSide(
-        //         color: theme.colorScheme.outlineVariant,
-        //         width: 1.0,
-        //       ),
-        //     ),
-        //   ),
-        //   child: BottomAppBar(
-        //     height: 64,
-        //     color: theme.colorScheme.surfaceContainerLow,
-        //     surfaceTintColor: theme.colorScheme.surfaceContainerLow,
-        //     child:
-        //         Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        //       IconButton(
-        //         icon: Icon(Icons.arrow_back_ios_new),
-        //         onPressed: () {
-        //           Navigator.pop(context);
-        //         },
-        //       ),
-        //       Expanded(
-        //         child: Padding(
-        //           padding: const EdgeInsets.all(8.0),
-        //         ),
-        //       ),
-        //       Row(
-        //         children: [
-        //           IconButton(
-        //             icon: Icon(Icons.bar_chart),
-        //             onPressed: () {},
-        //           ),
-        //           IconButton(
-        //             icon: Icon(Icons.share),
-        //             onPressed: () {
-        //               Share.share(
-        //                   appState.gameShareableUrl ?? "https://dailytrojan.com/games");
-        //             },
-        //           ),
-        //           IconButton(
-        //             icon: Icon(Icons.more_vert_sharp),
-        //             onPressed: () {},
-        //           ),
-        //         ],
-        //       ),
-        //     ]),
-        //   ),
-        // ),
-        );
+      ),
+    );
   }
 }
