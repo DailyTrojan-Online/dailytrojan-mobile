fe00768b59ab40e2182bba161640f40b8876e19a
Noah Pinales <pinales@usc.edu>
2026-03-02 16:11:09 -0800

    sharing now works within games

--- lib/game_route.dart
+++ lib/game_route.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 WebViewEnvironment? webViewEnvironment;
 
@@ -25,7 +26,17 @@ class GameRoute extends StatelessWidget {
                   isInspectable: kDebugMode,
                 ),
                 initialUrlRequest: URLRequest(url: WebUri(gameUrl ?? "")),
-                onWebViewCreated: (controller) {},
+                onWebViewCreated: (controller) {
+                  controller.addJavaScriptHandler(
+                      handlerName: 'requestShare',
+                      callback: (dynamic data) {
+                        // print arguments and other info coming from the JavaScript side!
+                        print(data[0]);
+
+                        SharePlus.instance.share(ShareParams(text: data[0]));
+
+                      });
+                },
                 onLoadStart: (controller, url) {},
                 onLoadStop: (controller, url) async {
                   await controller.evaluateJavascript(
